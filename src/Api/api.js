81d0762beb24cb74404e02a8b6036b531b285beb
zhaobo <zhaob@cdsihan.com>
2018-05-04 15:02:27 +0800

    在地图上添加塔的覆盖物

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,4 +1,5 @@
 const ApiUrl = {
-    fetchArea:'https://www.easy-mock.com/mock/5acad68fe416f67736f0284e/example/area'
+    fetchArea:'https://www.easy-mock.com/mock/5acad68fe416f67736f0284e/example/area',
+    fetchOneLine:'https://www.easy-mock.com/mock/5acad68fe416f67736f0284e/example/oneline'
 }
 export default ApiUrl;
--- src/components/map/map.vue
+++ src/components/map/map.vue
@@ -13,16 +13,12 @@ import ApiUrl from '@/Api/api.js';
 export default {
   data () {
       return {
-          
+          map:null,
+          g_icon14:''
       }
   },
   mounted () {
-      this.fetchArea().then(({xichangpointer})=>{
-          this.initMap(xichangpointer);
-      }).catch(err=>{
-
-      })
-      
+      this.fetchAll()
   },
   methods: {
       fetchArea(){
@@ -36,29 +32,79 @@ export default {
               })
           })
       },
-      initMap(areaArray){
-          var map = new TMap("map");
-          map.centerAndZoom(new TLngLat(102.272257,27.901112),7);
-          /*修改地图类型*/
-          map.setMapType(TMAP_HYBRID_MAP);
-          //开启滚轮缩放地图；
-          map.enableHandleMouseScroll();
-          //添加行政区轮廓图
-          this.outline(areaArray,map)
+      fetchOneline(){
+          return new Promise((resolve,reject)=>{
+              this.$axios.get(ApiUrl.fetchOneLine)
+              .then(({data})=>{
+                  resolve(data)
+              }).catch(err=>{
+                  reject(err)
+              })
+          })
       },
-      outline(aobj,map){
+      fetchAll(){
+          Promise.all([this.fetchArea(),this.fetchOneline()]).then((data)=>{
+              this.initMap(data[0].xichangpointer);
+              this.drawOneLine(data[1].lineJson);
+          }).catch(err=>{
+              this.$notify({
+                title: '警告',
+                message: '请求失败，请重试',
+                type: 'warning'
+             });
+          })
+      },
+      initMap(areaArray){
+          this.map = new TMap("map");
+          this.map.centerAndZoom(new TLngLat(102.272257,27.901112),7);
+          /*修改地图类型*/
+          this.map.setMapType(TMAP_HYBRID_MAP);
+          //开启滚轮缩放地图；
+          this.map.enableHandleMouseScroll();
+          //添加行政区轮廓图
+          this.outline(areaArray)
+      },
+      outline(aobj){
             var points = [];
             for (var k = 0; k < aobj.length; k++) {
                 var elem = aobj[k];
                 points.push(new TLngLat(elem[0], elem[1]));
             }
             //创建面对象
-            var polygon = new TPolygon(points, { strokeColor: "#00FFCF", strokeWeight: 6, strokeOpacity: 1, fillOpacity: 0 });
+            var polygon = new TPolygon(points, { strokeColor: "#00FFCF", strokeWeight: 4, strokeOpacity: 1, fillOpacity: 0 });
             //向地图上添加面
-            map.addOverLay(polygon);
+            this.map.addOverLay(polygon);
       },
-      drawOneLine(){
-          
+      drawOneLine(linearr){
+          var alllineObj = [];
+          for (var i = 0; i < linearr.length; i++) {
+            var oneline = linearr[i];
+            var oneobj = [];
+            this.g_icon14 = this.iconObjfn('/static/img/ta.png',30,40,15,15);
+            for(let j = 0;j<oneline.pointer.length;j++)
+            {
+                var elem = oneline.pointer[j];
+                oneobj.push(new TLngLat(elem.coord[0], elem.coord[1]));
+                var marker_14 = new TMarker(new TLngLat(elem.coord[0], elem.coord[1]), {
+					icon: this.g_icon14
+				});
+                this.map.addOverLay(marker_14)
+            }
+          }
+          alllineObj.push(oneobj);
+          for (var i = 0; i < alllineObj.length; i++) {
+			var line_color = '#2196F3';
+			var oneline = alllineObj[i];
+			var line = new TPolyline(oneline, {
+				strokeColor: line_color,
+				strokeWeight: 3,
+				strokeOpacity: 1
+			});
+			this.map.addOverLay(line);
+		}
+      },
+      iconObjfn(pic_url, w, h, x, y){
+          return new TIcon(pic_url, new TSize(w, h), { anchor: new TPixel(x, y) });
       }
   }
 }
